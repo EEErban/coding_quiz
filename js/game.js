b4ba03aabd19941a6b0ca5912efa688407b1e001
Farah Erban <f-erban@Farah-Erbans-MacBook-Pro.local>
2021-03-26 15:06:07 +0000

    question variables added

--- js/game.js
+++ js/game.js
@@ -1,5 +1,23 @@
 const question = document.querySelector('#question')
-const choices = document.querySelectorAll('.choice-text')
-const question = document.querySelector('#question')
-const question = document.querySelector('#question')
-const question = document.querySelector('#question')
+const choices = Array.from(document.querySelectorAll('.choice-text')
+const progressText = document.querySelector('#question')
+const scoreText = document.querySelector('#score')
+const progressBarFull = document.querySelector('#progressBarFull')
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let avaiableQuestions = []
+
+let questions = [
+    {
+         question: 'Who were the duo that founded Nike?',
+         choice1: 'Harold Humphreys & Wallace Humphreys',
+         choice2: 'Adolf Dassler & Rudolph Dassler',
+         choice3: 'Bill Bowerman & Phil Knight',
+         choice4: 'Joseph W. Foster & Jeff Foster',
+         answer: Bill Bowerman & Phil Knight,
+
+    }
+]
